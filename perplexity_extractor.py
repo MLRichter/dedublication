a641e6feb0fd32f648aefbcdd52244d57526ff87
MLRichter <matrichter@uos.de>
2023-02-06 19:10:55 -0500

    requirements

--- perplexity_extractor.py
+++ perplexity_extractor.py
@@ -20,6 +20,7 @@ dataset_mapper = {"c4_15M": lambda: datasets.load_dataset('teven/c4_15M', "binar
 
 def obtain_dataset(sample_start: int = 0, sample_end: int = None, dataset_key = "c4_15M"):
     global DATASET
+    print("Fetching dataset", dataset_key)
     if DATASET is None:
         ds = dataset_mapper[dataset_key]()
         DATASET = ds
