f984378577e664833c62586ef442e9493846d58b
MLRichter <matrichter@uos.de>
2023-02-09 15:18:47 -0500

    duplicate removal added

--- test_s3_loading.py
+++ test_s3_loading.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset_builder
 import datasets
-data_files = {"train": "s3://s-laion/bild_text/run1/2023-02-07-23-32-48/part_1/*.parquet"}
+data_files = {"*.parquet"}
 output_dir = "s3://s-laion/bild_text/run1/2023-02-07-23-32-48/"
 builder = load_dataset_builder("parquet", data_files=data_files)
 s3 = datasets.filesystems.S3FileSystem(anon=False)
