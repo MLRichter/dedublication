237b9e10370a82189fd75cd88a9b241f9875c07b
MLRichter <matrichter@uos.de>
2023-02-13 18:46:42 -0500

    duplicate removal added

---
+++ load_files_from_s3.py
@@ -0,0 +1,38 @@
+from typing import Tuple, List
+
+import boto3
+import tqdm
+from smart_open import smart_open
+
+
+# example: s3://s-laion/bild_text/run1/2023-02-07-23-32-48/
+def split_s3_path(path: str) -> Tuple[str, str]:
+    bucket_with_prefix = path.split("://")[-1]
+    bucket, prefix = bucket_with_prefix.split("/", maxsplit=1)
+    return bucket, prefix
+
+
+def list_all_files_with_prefix(bucket, prefix, filext=None):
+    s3 = boto3.resource('s3')
+    result = s3.list_objects(Bucket=bucket, Prefix=prefix, Delimiter='/')
+    filenames = []
+    for cont in tqdm.tqdm(result["Contents"], "Reading files from S3"):
+        filename = cont["Key"]
+        if filext is not None and not filename.endswith(filext):
+            continue
+        filenames.append(filename)
+    return filenames
+
+
+def reassemble_uri(bucket: str, filename: str):
+    return rf"s3://{bucket}/{filename}"
+
+
+def reassemble_uris(bucket: str, filenames: List[str]):
+    return [reassemble_uri(bucket, filename) for filename in filenames]
+
+
+def s3_listdir(s3_path: str):
+    bucket, prefix = split_s3_path(s3_path)
+    filenames = list_all_files_with_prefix(bucket, prefix)
+    return reassemble_uris(bucket=bucket, filenames=filenames)
