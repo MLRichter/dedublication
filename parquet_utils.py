46473ce3a0abb30af5e61e5e48c99737120bcf87
MLRichter <matrichter@uos.de>
2023-02-15 20:57:22 -0500

    duplicate removal added

--- parquet_utils.py
+++ parquet_utils.py
@@ -47,7 +47,7 @@ def _indexing_files(length_map: Dict[str, int]):
 
 class SharededParquetS3Dataset:
 
-    def __init__(self, s3_url: str, hash_idx: int = 0, uri_index: int = 1, text_idx: int = 2, cache_dir: str = "./s3filecache"):
+    def __init__(self, s3_url: str, hash_idx: int = 0, uri_index: int = 1, text_idx: int = 2, cache_dir: str = "./s3filecache", batch_size: int = 10000):
         files = s3_listdir(s3_url, ".parquet")
         # ensure the files are allways the same
         files.sort()
@@ -56,10 +56,13 @@ class SharededParquetS3Dataset:
         self.index_map = _indexing_files(self.length_map)
         self.cache = None
         self.cache_name = None
+        self.cache_ed_indices = None
         self.hash_index = hash_idx
         self.uri_index = uri_index
         self.text_idx = text_idx
         self.s3 = boto3.client('s3')
+        self.batch_size = batch_size
+        self.index_range = None
 
     def _check_for_index_in_infile(self, idx: int, file: str) -> bool:
         return idx >= self.index_map[file][0] and idx < self.index_map[file][1]
@@ -80,18 +83,31 @@ class SharededParquetS3Dataset:
         assert true_index >= 0
         return true_index
 
+
+    def _check_index_cache_hit(self, true_index: int):
+        if self.index_range is None:
+            return False
+        else:
+            return true_index >= self.index_range[0] and true_index < self.index_range[1]
+
     def _obtain_with_true_index(self, true_index: int, file: str):
         # check if cache miss; load if necessary
-        if file != self.cache_name:
-            table = pq.read_table(file)
+        if file != self.cache_name or self._check_index_cache_hit(true_index=true_index):
+            self.index_range = (true_index,  true_index + self.batch_size)
+            table = pq.read_table(file)[self.index_range[0]:self.index_range[1]]
             self.cache = table
             self.cache_name = file
 
-        return {
-            "hash": self.cache[self.hash_index][true_index].as_py(),
-            "uri": self.cache[self.uri_index][true_index].as_py(),
-            "text": self.cache[self.text_idx][true_index].as_py()
+        # batch index is the true index relative to the start of the batch window
+        batch_index = true_index - self.index_range[0]
+        assert true_index >= 0
+
+        data_point =  {
+            "hash": self.cache[self.hash_index][batch_index].as_py(),
+            "uri": self.cache[self.uri_index][batch_index].as_py(),
+            "text": self.cache[self.text_idx][batch_index].as_py()
         }
+        return data_point
 
     def __getitem__(self, idx):
         file = self._find_correct_file(idx)
